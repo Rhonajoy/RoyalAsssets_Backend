a0d12368122eca0430984a2f3f158963f6bc065e
RhonaJoyKe <rhonajoy.koome@student.moringaschool.com>
2022-01-24 13:53:42 +0300

    creating a profile signal

--- app/models.py
+++ app/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
+    full_name=models.CharField(max_length=50, blank=True, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email=models.EmailField(max_length = 200)
 
     profile_photo = CloudinaryField('image')
 
-    bio = models.TextField(max_length=500, blank=True, null=True)
+   
 
     contact = models.CharField(max_length=50, blank=True, null=True)
 
@@ -20,4 +24,12 @@ class Profile(models.Model):
 
     def delete_profile(self):
         self.delete()
+     @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
