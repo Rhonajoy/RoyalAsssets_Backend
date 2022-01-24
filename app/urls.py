7173accfd6dbfaca285b6b5621c88e5ec8b6d800
RhonaJoyKe <rhonajoy.koome@student.moringaschool.com>
2022-01-24 14:21:31 +0300

    creates url for profile

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+from django.urls import path
+urlpatterns=[
+ path('api/profile/', views.ProfileList.as_view(),name='')
+   
+
+]
