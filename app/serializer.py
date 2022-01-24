d6ba0d62de8566c3ae9cbc23950cf6c8ec81f88b
RhonaJoyKe <rhonajoy.koome@student.moringaschool.com>
2022-01-24 19:29:22 +0300

    creates user view

--- app/serializer.py
+++ app/serializer.py
@@ -3,8 +3,8 @@ from .models import Profile,User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
-        fields=('')
+        fields=('username','email','password','passwordconfirmation')
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('profile_photo', 'full_name', 'username','role')
+        fields = ('profile_photo', 'full_name', 'user','role')
--- app/views.py
+++ app/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .serializer import ProfileSerializer
+from .serializer import ProfileSerializer,UserSerializer
 from rest_framework.response import Response
 from .models import Profile,User
+from rest_framework.response import status 
 
 # Create your views here.
+class user:
+       @APIView(['POST']) 
+       def postuser(self, request, format=None):
+        serializers = UserSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@APIView(['GET'])
-def profilelist(self, request, format=None):
-        all_profiles = Profile.objects.all()
-        serializers = ProfileSerializer(all_profiles, many=True)
-        return Response(serializers.data)
-@APIView(['GET'])
-def singleprofile(username, format=None):
-        # user = User.objects.filter(username=username).first
-        profile=Profile.objects.filter(username=username)
-        serializers = ProfileSerializer(profile, many=False)
-        return Response(serializers.data)
-    
-    
+
+class profile:
+        @APIView(['GET'])
+        def profilelist(self, request, format=None):
+                all_profiles = Profile.objects.all()
+                serializers = ProfileSerializer(all_profiles, many=True)
+                return Response(serializers.data)
+        @APIView(['GET'])
+        def singleprofile(username, format=None):
+                user = User.objects.filter(username=username).first
+                profile=Profile.objects.filter(user=user)
+                serializers = ProfileSerializer(profile, many=False)
+                return Response(serializers.data)
+        
+        
 
