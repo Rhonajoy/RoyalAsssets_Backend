cb8077420f27d41d3808127eca83ea5fdac945cf
RhonaJoyKe <rhonajoy.koome@student.moringaschool.com>
2022-01-31 16:24:46 +0300

    modifies models

--- app/models.py
+++ app/models.py
@@ -110,7 +110,7 @@ class Asset(models.Model):
     name = models.CharField(max_length=100, null=True)
     category = models.CharField(max_length=50, choices=CATEGORY, null=True)
     asset_value = models.PositiveIntegerField(null=True)
-    quantity=models.PositiveIntegerField()
+    quantity=models.IntegerField()
     
 
 
