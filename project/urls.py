9bb209ab38cd3ac9c970278753b75d9684914914
RhonaJoyKe <rhonajoy.koome@student.moringaschool.com>
2022-01-31 09:52:17 +0300

    documentation

--- project/urls.py
+++ project/urls.py
@@ -30,24 +30,24 @@ from django.conf.urls import url, include
 
 
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Rhonajoy API",
-        default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="joy@gmail.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="RoyalAssets API",
+#         default_version="v1",
+#         description="Test description",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="joy@gmail.com"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 
 urlpatterns = [
-    path("swagger/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    # path("swagger/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    # path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    # path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("admin/", admin.site.urls),
     url(r"^", include("app.urls")),
     # path('api-token-auth/', views.obtain_auth_token)
